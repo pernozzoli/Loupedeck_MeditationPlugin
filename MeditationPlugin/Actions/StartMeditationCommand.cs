5012ae7a0d393f998868882ae70d7963836188d5
adpear0 <werbung@pernozzoli.de>
2023-01-12 10:07:37 +0100

    feat: initial commit

---
+++ MeditationPlugin/Actions/StartMeditationCommand.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Timers;
+
+namespace Loupedeck.MeditationPlugin.Actions
+{
+    public class StartMeditationCommand : PluginDynamicCommand
+    {
+        protected int Minutes { get; set; } = 5;
+        public MeditationPlugin MeditationPlugin => this.Plugin as MeditationPlugin;
+
+        protected DateTimeOffset _startTime;
+        protected TimeSpan _timeElapsed = TimeSpan.Zero;
+        protected bool _timerStarted = false;
+
+        public StartMeditationCommand() : base("Start Meditation", "Starts a meditation with a given time", "Meditations")
+        {
+            this.MakeProfileAction("text;Meditation time:");
+        }
+
+        /// <summary>
+        /// Run the command, given either a defined number of minutes or 5 minutes as default
+        /// </summary>
+        /// <param name="actionParameter">Action parameter containing meditation interval</param>
+        protected override void RunCommand(String actionParameter)
+        {
+            int minutes = 0;
+            if (int.TryParse(actionParameter, out minutes))
+            {
+                Minutes = minutes;
+            }
+            else
+            {
+                Minutes = 5;
+            }
+
+            /// Start gong
+            MeditationPlugin.PlayGong();            _startTime = DateTimeOffset.Now;
+
+            /// Get the meditation time set
+            if (!_timerStarted)
+            {
+                _timerStarted = true;
+                MeditationPlugin.Tick += this.MeditationPlugin_Tick;
+            }
+        }
+
+        private void MeditationPlugin_Tick(Object sender, EventArgs e)
+        {
+            _timeElapsed = DateTimeOffset.Now - _startTime;
+            if (_timeElapsed.TotalMinutes >= Minutes)
+            {
+                MeditationPlugin.Tick -= this.MeditationPlugin_Tick;
+                /// Stop gong
+                MeditationPlugin.PlayGong();
+                _timerStarted = false;
+            }
+
+            this.ActionImageChanged("");
+        }
+
+
+        /// <summary>
+        /// Gets the command image depending on if timer is running or paused / stopped
+        /// </summary>
+        /// <param name="actionParameter">Action parameter containing the meditation interval</param>
+        /// <param name="imageSize">Icon image size</param>
+        /// <returns>The bitmap image, including remaining time if meditation is running</returns>
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
+        {
+            if (_timerStarted)
+            {
+                using (BitmapBuilder bitmapBuilder = new BitmapBuilder(imageSize))
+                {
+                    var fn = EmbeddedResources.FindFile("meditation_running_80.png");
+                    BitmapImage runningIcon = EmbeddedResources.ReadImage(fn);
+                    var x1 = bitmapBuilder.Width * 0.1;
+                    var w = bitmapBuilder.Width * 0.8;
+                    var y1 = bitmapBuilder.Height * 0.45;
+                    var y2 = bitmapBuilder.Height * 0.65;
+                    var h = bitmapBuilder.Height * 0.3;
+                    bitmapBuilder.DrawImage(runningIcon);
+                    bitmapBuilder.DrawText((TimeSpan.FromMinutes(Minutes) - _timeElapsed).ToString(@"mm\:ss"), (Int32)x1, (Int32)y2, (Int32)w, (Int32)h, BitmapColor.White, imageSize == PluginImageSize.Width90 ? 15 : 9, imageSize == PluginImageSize.Width90 ? 2 : 2);
+                    //bitmapBuilder.DrawText("Test", (Int32)x1, (Int32)y2, (Int32)w, (Int32)h, BitmapColor.White, imageSize == PluginImageSize.Width90 ? 15 : 9, imageSize == PluginImageSize.Width90 ? 2 : 2);
+                    return bitmapBuilder.ToImage();
+                    //return runningIcon;
+                }
+            }
+            else
+            {
+                var fn = EmbeddedResources.FindFile("meditation_paused_80.png");
+                return EmbeddedResources.ReadImage(fn);
+            }
+        }
+    }
+}
+
---
+++ MeditationPlugin/MeditationPlugin.cs
@@ -0,0 +1,105 @@
+namespace Loupedeck.MeditationPlugin
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Runtime.InteropServices.ComTypes;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using NAudio.Wave;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using Newtonsoft.Json.Serialization;
+
+    // This class contains the plugin-level logic of the Loupedeck plugin.
+
+    public class MeditationPlugin : Plugin
+    {
+        // Gets a value indicating whether this is an Universal plugin or an Application plugin.
+        public override Boolean UsesApplicationApiOnly => true;
+
+        // Gets a value indicating whether this is an API-only plugin.
+        public override Boolean HasNoApplication => true;
+
+        public string GongPath
+        {
+            get
+            {
+                var pluginDataDirectory = this.GetPluginDataDirectory();
+                var gongPath = Path.Combine(pluginDataDirectory, "gong.wav");
+
+                if (IoHelpers.EnsureDirectoryExists(pluginDataDirectory))
+                {
+                    if (!File.Exists(gongPath))
+                    {
+                        Stream soundStream = EmbeddedResources.GetStream("Loupedeck.MeditationPlugin.Resources.bell-sound.wav");
+                        using (Stream destination = File.Create(gongPath))
+                        {
+                            soundStream.CopyTo(destination);
+                        }
+                    }
+                }
+
+                return gongPath;
+            }
+        }
+
+
+        public event EventHandler<EventArgs> Tick;
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private async void Timer()
+        {
+            while (true && !this._cancellationTokenSource.IsCancellationRequested)
+            {
+                await Task.Delay(1000);
+                Tick?.Invoke(this, new EventArgs());
+            }
+
+        }
+
+        // This method is called when the plugin is loaded during the Loupedeck service start-up.
+        public override void Load()
+        {
+            this.Timer();
+        }
+
+        // This method is called when the plugin is unloaded during the Loupedeck service shutdown.
+        public override void Unload()
+        {
+            this._cancellationTokenSource.Cancel();
+        }
+
+        public void PlayGong()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                FileName = "/usr/bin/afplay",
+                Arguments = GongPath,
+            };
+            Process proc = new Process()
+            {
+                StartInfo = startInfo,
+            };
+            proc.Start();
+
+            //using (Stream music = this.GongStream)
+            //{
+            //    music.Seek(0, SeekOrigin.Begin);
+            //    System.Media.SoundPlayer soundPlayer = new System.Media.SoundPlayer(this.GongStream);
+            //    soundPlayer.Play();
+            //}
+            //StreamMediaFoundationReader reader = new StreamMediaFoundationReader(this.GongStream);
+            //using (var outputDevice = new WaveOutEvent())
+            //{
+            //    outputDevice.Init(reader);
+            //    outputDevice.Play();
+            //    //while (outputDevice.PlaybackState == PlaybackState.Playing)
+            //    //{
+            //    //    Thread.Sleep(1000);
+            //    //}
+            //}
+        }
+    }
+}
